67b61531dce323dbcdac06f187fc2e8239272ef0
Cesar <cesarcamposdelima@gmail.com>
2020-04-27 20:59:54 -0300

    Adicionada classe FornecedorRepositorio

---
+++ src/Infra/Repositories/FornecedorRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Domain.Fornecedores;
+using Infra.Contexts;
+
+namespace Infra.Repositories
+{
+    public class FornecedorRepositorio : IFornecedorRepositorio
+    {
+        private readonly FornecedoresContext _context;
+
+        public FornecedorRepositorio(FornecedoresContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task Salvar(Fornecedor fornecedor)
+        {
+            await _context.Fornecedors.AddAsync(fornecedor);
+        }
+    }
+}
