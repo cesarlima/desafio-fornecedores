2b6df01428bd898a33b169c3aa13cba72272b1e2
Cesar <cesarcamposdelima@gmail.com>
2020-04-29 17:31:32 -0300

    Validar resultado do cadastro de empresa

--- tests/UnitTests/CasosDeUso/CadastrarEmpresa/CadastrarEmpresaCasoDeUsoTest.cs
+++ tests/UnitTests/CasosDeUso/CadastrarEmpresa/CadastrarEmpresaCasoDeUsoTest.cs
@@ -14,26 +14,26 @@ namespace UnitTests.CasosDeUso.CadastrarEmpresa
     {
         private readonly Mock<IOutputPort> _empresaPresenterMock = new Mock<IOutputPort>();
         private Mock<IEmpresaRepositorio> _empresaRepositorioMock = new Mock<IEmpresaRepositorio>();
-        private Mock<IUnitOfWork> _uow = new Mock<IUnitOfWork>();
+        private Mock<IUnitOfWork> _uowMock = new Mock<IUnitOfWork>();
+        private Mock<IEmpresaFactory> _empresaFactoryMock = new Mock<IEmpresaFactory>();
         private IEmpresaFactory _empresaFactory = new EntidadeFactories();
-
 
         [Fact]
         public void Deve_Disparar_ArgumentNullException_Caso_IOutputPort_For_Null()
         {
-            Assert.Throws<ArgumentNullException>(() => new CadastrarEmpresaCasoDeUso(null, _empresaRepositorioMock.Object, _empresaFactory, _uow.Object));
+            Assert.Throws<ArgumentNullException>(() => new CadastrarEmpresaCasoDeUso(null, _empresaRepositorioMock.Object, _empresaFactory, _uowMock.Object));
         }
 
         [Fact]
         public void Deve_Disparar_ArgumentNullException_Caso_IEmpresaRepositorio_For_Null()
         {
-            Assert.Throws<ArgumentNullException>(() => new CadastrarEmpresaCasoDeUso(_empresaPresenterMock.Object, null, _empresaFactory, _uow.Object));
+            Assert.Throws<ArgumentNullException>(() => new CadastrarEmpresaCasoDeUso(_empresaPresenterMock.Object, null, _empresaFactory, _uowMock.Object));
         }
 
         [Fact]
         public async Task Deve_Adicionar_Notificacao_Se_CNPJ_Invalido()
         {
-            var sut = new CadastrarEmpresaCasoDeUso(_empresaPresenterMock.Object, _empresaRepositorioMock.Object, _empresaFactory, _uow.Object);
+            var sut = CriarSUT();
             await sut.Execute(new CadastrarEmpresaInput("any_uf", "any_empresa", "888"));
             var notificacoes = new List<string>() { "CNPJ inválido" };
             _empresaPresenterMock.Verify(presenter => presenter.AddNotifications(notificacoes), Times.Once());
@@ -42,7 +42,7 @@ namespace UnitTests.CasosDeUso.CadastrarEmpresa
         [Fact]
         public async Task Nao_Deve_Adicionar_Notificacao_Se_CNPJ_Valido()
         {
-            var sut = new CadastrarEmpresaCasoDeUso(_empresaPresenterMock.Object, _empresaRepositorioMock.Object, _empresaFactory, _uow.Object);
+            var sut = CriarSUT();
             await sut.Execute(new CadastrarEmpresaInput("any_uf", "any_empresa", "33.194.965/0001-11"));
 
             _empresaPresenterMock.Verify(presenter => presenter.AddNotification("CNPJ inválido"), Times.Never());
@@ -51,26 +51,45 @@ namespace UnitTests.CasosDeUso.CadastrarEmpresa
         [Fact]
         public async Task Deve_Adicionar_Notificacao_Se_CNPJ_Ja_Cadastrado()
         {
-            var cnpj = new Domain.Common.ValueObjects.CNPJ("33.194.965/0001-11");
-            var sut = new CadastrarEmpresaCasoDeUso(_empresaPresenterMock.Object, _empresaRepositorioMock.Object, _empresaFactory, _uow.Object);
-            _empresaRepositorioMock.Setup(rep => rep.EmpresaJaCadastrada(cnpj)).ReturnsAsync(true);
-            await sut.Execute(new CadastrarEmpresaInput("any_uf", "any_empresa", cnpj.ToString()));
+            var input = new CadastrarEmpresaInput("any_uf", "any_empresa", "33.194.965/0001-11");
+            var sut = CriarSUT();
+            _empresaRepositorioMock.Setup(rep => rep.EmpresaJaCadastrada(new Domain.Common.ValueObjects.CNPJ(input.CNPJ))).ReturnsAsync(true);
+
+            await sut.Execute(input);
 
             _empresaPresenterMock.Verify(presenter => presenter.AddNotification("CNPJ já cadastrado"), Times.Once());
         }
 
         [Fact]
-        public async Task Deve_Adicionar_Result_Se_CNPJ_Ja_Cadastrado()
+        public async Task Deve_Adicionar_CadastrarEmpresaOutput_Corretamente_Se_Empresa_Cadastrada_Com_Sucesso()
         {
-            const string cnpj = "33.194.965/0001-11";
-            var sut = new CadastrarEmpresaCasoDeUso(_empresaPresenterMock.Object, _empresaRepositorioMock.Object, _empresaFactory, _uow.Object);
+            var input = new CadastrarEmpresaInput("any_uf", "any_empresa", "33.194.965/0001-11");
+            var empresa = _empresaFactory.NovaEmpresa(input.UF, input.NomeFantasia, input.CNPJ);
+
+            _empresaFactoryMock.Setup(f => f.NovaEmpresa(input.UF, input.NomeFantasia, input.CNPJ)).Returns(empresa);
+            
             CadastrarEmpresaOutput result = null;
             _empresaPresenterMock.Setup(presenter => presenter.AddResult(It.IsAny<CadastrarEmpresaOutput>()))
             .Callback((CadastrarEmpresaOutput r) => result = r);
 
-            await sut.Execute(new CadastrarEmpresaInput("any_uf", "any_empresa", cnpj));
+            var sut = CriarSUT(empresaFactoryMock: _empresaFactoryMock);
+            await sut.Execute(input);
 
-            Assert.NotNull(result);
+            Assert.Equal(empresa.Id, result?.Id);
+            Assert.Equal(empresa.UF, result?.UF);
+            Assert.Equal(empresa.NomeFantasia, result?.NomeFantasia);
+            Assert.Equal(empresa.CNPJ.ToString(), result?.CNPJ);
+        }
+
+        private CadastrarEmpresaCasoDeUso CriarSUT(Mock<IOutputPort> empresaPresenterMock = null,
+                                                   Mock<IEmpresaRepositorio> empresaRepositorioMock = null,
+                                                   Mock<IUnitOfWork> uowMock = null,
+                                                   Mock<IEmpresaFactory> empresaFactoryMock = null)
+        {
+            return new CadastrarEmpresaCasoDeUso(empresaPresenterMock?.Object ?? _empresaPresenterMock.Object,
+                                                 empresaRepositorioMock?.Object ?? _empresaRepositorioMock.Object,
+                                                 empresaFactoryMock?.Object ?? _empresaFactory,
+                                                 uowMock?.Object ?? _uowMock.Object);
         }
     }
 }
