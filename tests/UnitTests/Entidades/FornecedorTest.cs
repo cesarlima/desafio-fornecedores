1065acca6d1f37b6962509fa49fef126f8807ed2
Cesar <cesarcamposdelima@gmail.com>
2020-04-29 23:15:09 -0300

    Adicionado testes para o Fornecedor

---
+++ tests/UnitTests/Entidades/FornecedorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Common.ValueObjects;
+using Domain.Empresas;
+using Domain.Fornecedores;
+using Xunit;
+
+namespace UnitTests.Entidades
+{
+    public class FornecedorTest
+    {
+        [Fact]
+        public void Deve_Adicionar_Notificacao_Quando_Fornecedor_Pessoa_Fisica_Do_Parana_Menor_De_Idade()
+        {
+            var empresa = new Empresa("PR", "nome", new CNPJ("11672897000116"));
+            var pessoaFisica = new PessoaFisica("nome", "rg", new DateTime(2003, 5, 8), new CPF("52647680051"));
+            var sut = new Fornecedor(pessoaFisica, empresa);
+
+            var esperado = new List<string>() { "Não é permitido cadastrar fornecedor pessoa física menor de idade" };
+
+            Assert.Equal(esperado, sut.Notificacoes);
+        }
+    }
+}
